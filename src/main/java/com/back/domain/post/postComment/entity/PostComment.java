872c8a92a51a44801be1e3354fc86cf1554a8252
jooky <jooky0327@naver.com>
2025-09-03 12:33:38 +0900

    comment 추가

---
+++ src/main/java/com/back/domain/post/postComment/entity/PostComment.java
@@ -0,0 +1,27 @@
+package com.back.domain.post.postComment.entity;
+
+import com.back.domain.post.post.entity.Post;
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class PostComment extends BaseEntity {
+    private String content;
+
+    @ManyToOne
+    private Post post;
+
+    public PostComment(Post post, String content) {
+        this.post = post;
+        this.content = content;
+    }
+
+    public void modify(String content) {
+        this.content = content;
+    }
+}
