a2ddfc475168dd07d9d54157f58f50dc23e8e538
jooky <jooky0327@naver.com>
2025-09-03 11:41:29 +0900

    필수 service 함수 추가

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -1,11 +1,37 @@
 package com.back.domain.post.post.service;
 
+import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+
+    public long count() {
+        return 0;
+    }
+
+    public Post create(String title, String content) {
+        Post post = new Post(title, content);
+        return postRepository.save(post);
+    }
+
+    public void update(Post post, String title, String content) {
+        post.modify(title, content);
+    }
+
+    public List<Post> getList() {
+        return postRepository.findAll();
+    }
+
+    public Post getPost(Long id) {
+        return postRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("게시글이 존재하지 않습니다.")
+        );
+    }
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -14,7 +14,12 @@ public class BaseInitData {
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
+            if(postService.count() > 0) return;
 
+            postService.create("제목 1", "내용 1");
+            postService.create("제목 2", "내용 2");
+            postService.create("제목 3", "내용 3");
         };
     }
+
 }
