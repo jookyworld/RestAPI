cfd23eaecc4c104b4ac101f52366ebaceb5b7870
jooky <jooky0327@naver.com>
2025-09-12 15:16:26 +0900

    회원 도메인 추가

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.global.initData;
 
+import com.back.domain.member.member.service.MemberService;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -19,16 +20,29 @@ public class BaseInitData {
     private BaseInitData self;
 
     private final PostService postService;
+    private final MemberService memberService;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
             self.work1();
+            self.work2();
         };
     }
 
     @Transactional
     public void work1() {
+        if(memberService.count() > 0 ) return;
+
+        memberService.join("user1", "1234", "유저1");
+        memberService.join("user2", "1234", "유저2");
+        memberService.join("user3", "1234", "유저3");
+        memberService.join("user4", "1234", "유저4");
+        memberService.join("user5", "1234", "유저5");
+    }
+
+    @Transactional
+    public void work2() {
         if(postService.count() > 0) return;
 
         Post post1 = postService.create("제목 1", "내용 1");
@@ -40,6 +54,12 @@ public class BaseInitData {
         post1.addComment("댓글 1-3");
         post2.addComment("댓글 2-1");
         post2.addComment("댓글 2-2");
+    }
+
+
+    @Transactional
+    public void work3() {
+
     }
 
 }
